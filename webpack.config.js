867a1fc05809508c2abf8ae43930a68e34e9b77d
Brandon Patterson <brandnpatterson@gmail.com>
2018-08-25 01:39:55 -0500

    remove unused files

--- webpack.config.js
+++ webpack.config.js
@@ -6,70 +6,70 @@ let StyleLintPlugin = require('stylelint-webpack-plugin');
 let devMode = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '/public/'),
-        compress: true,
-        open: true,
-        stats: 'errors-only',
-        watchContentBase: true
-    },
-    entry: './src/js/index.js',
-    output: {
-        path: path.resolve(__dirname, './public/'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'eslint-loader'
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer]
-                        }
-                    },
-                    'sass-loader'
-                ]
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/public/'),
+    compress: true,
+    open: true,
+    stats: 'errors-only',
+    watchContentBase: true
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './public/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader'
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer]
             }
+          },
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            minify: {
-                collapseWhitespace: true
-            },
-            filename: 'index.html',
-            hash: true,
-            template: path.join(__dirname, '/src/') + 'template.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new StyleLintPlugin({
-            files: './src/scss/**/*.scss',
-            syntax: 'scss'
-        })
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true
+      },
+      filename: 'index.html',
+      hash: true,
+      template: path.join(__dirname, '/src/') + 'template.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new StyleLintPlugin({
+      files: './src/scss/**/*.scss',
+      syntax: 'scss'
+    })
+  ]
 };
